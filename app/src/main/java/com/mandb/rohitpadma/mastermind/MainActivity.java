6203f9b0c7481dcd76db3906ae16c5a976b78cac
Rohit Padma <rohitp32@gmail.com>
2019-11-17 01:39:12 -0500

    Initial project setup

---
+++ app/src/main/java/com/mandb/rohitpadma/mastermind/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mandb.rohitpadma.mastermind;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
